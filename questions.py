bde5d3c75765f1207a0ba2b2cf1a4961592c42ba
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-29 00:02:05 +0800

    forgot the class of questions, use contact details as guide for questions button

--- questions.py
+++ questions.py
@@ -1,34 +1,43 @@
     # Prompt each frame for each number
-    # 1. Have you been vaccinated for COVID-19?
-        # Not yet
-        # 1st Dose
-        # 2nd Dose (Fully Vaccinated)
-        # 1st (Booster)
-        # 2nd (Completed Booster Shot)
-    # 2. Do you experience any COVID like symptoms in the past 7 days such as:
-        # Fever
-        # Cough
-        # Colds
-        # Muscle Cramps
-        # sore throat
-        # Diarrhea
-        # Headache
-        # Shortness of breath
-        # Difficulty in breathing
-        # Loss of Taste
-        # Loss of Smell
-        # None of the above
-    # 3. Have you had exposure to a probable confirmed COVID cases in the last 14 days?
-        # No
-        # Yes
-    # 4. Have you had contact with COVID like symptoms in the past 7 days?
-        # NO
-        # Yes
-    # 5. Have you been tested for COVID-19 in the last 14 days?
-        # No
-        # Yes-postive
-        # Yes-Negative
-        # Yes-Pending
+import tkinter as tk
+from tkinter import messagebox
+
+class QuestionsFrame(tk.Frame):
+    def __init__(self, master, back_callback):
+        super().__init__(master)
+        self.master = master
+        self.back_callback = back_callback
+
+        self.questions = [
+            "Have you been vaccinated for COVID-19?",
+            "Do you experience any COVID-like symptoms in the past 7 days such as:",
+            "Have you had exposure to a probable confirmed COVID cases in the last 14 days?",
+            "Have you had contact with COVID-like symptoms in the past 7 days?",
+            "Have you been tested for COVID-19 in the last 14 days?"
+        ]
+
+        self.options = [
+            ["Not yet", "1st Dose", "2nd Dose (Fully Vaccinated)", "1st (Booster)", "2nd (Completed Booster Shot)"],
+            ["Fever", "Cough", "Colds", "Muscle Cramps", "Sore Throat", "Diarrhea", "Headache", "Shortness of Breath",
+             "Difficulty in Breathing", "Loss of Taste", "Loss of Smell"],
+            ["No", "Yes"],
+            ["No", "Yes"],
+            ["No", "Yes - Positive", "Yes - Negative", "Yes - Pending"]
+        ]
+
+        self.answers = []
+        self.current_question = 0
+
+        self.selected_option = tk.StringVar()
+
+        self.question_label = tk.Label(self, text=self.questions[self.current_question])
+        self.question_label.grid(row=0, column=0, padx=10, pady=5, columnspan=2)
+
+        self.option_frame = tk.Frame(self)
+        self.option_frame.grid(row=1, column=0, padx=10, pady=5, columnspan=2)
+
+        self.next_button = tk.Button(self, text="Next", command=self.save_answer)
+        self.next_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
     # prompt only 
         #if in number 3-4 = "yes" and in number 5 ="Yes-Positve or Yes-pending"
