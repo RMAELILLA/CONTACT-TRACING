e628fa15e6c76ba51d59d506b5cf00505e3d87c9
RMAELILLA <ronioryzamae12@gmail.com>
2023-07-29 11:14:06 +0800

    separate the file for questions2, use questions file for guide

---
+++ questions2.py
@@ -0,0 +1,50 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class AdditionalQuestionsFrame(tk.Toplevel):
+    def __init__(self, master, back_callback):
+        super().__init__(master)
+        self.master = master
+        self.back_callback = back_callback
+
+        self.questions = [
+            "When was your most visit to this location?",
+            "Since then until today, what places have you been? (besides home)"
+        ]
+
+        self.answers = []
+        self.current_question = 0
+
+        self.question_label = tk.Label(self, text=self.questions[self.current_question])
+        self.question_label.grid(row=0, column=0, padx=10, pady=5, columnspan=2)
+
+        self.answer_entry = tk.Entry(self)
+        self.answer_entry.grid(row=1, column=0, padx=10, pady=5, columnspan=2)
+
+        self.next_button = tk.Button(self, text="Next", command=self.save_answer)
+        self.next_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+    def save_answer(self):
+        answer = self.answer_entry.get()
+        if answer:
+            self.answers.append(answer)
+            self.answer_entry.delete(0, tk.END)
+            self.show_next_question()
+        else:
+            messagebox.showerror("Error", "Please enter an answer for the current question.")
+
+    def show_next_question(self):
+        if self.current_question < len(self.questions) - 1:
+            self.current_question += 1
+            self.question_label.config(text=self.questions[self.current_question])
+        else:
+            self.display_contact_details()
+
+    def display_contact_details(self):
+        message = "Additional Answers:\n"
+        for i in range(len(self.questions)):
+            message += f"{self.questions[i]}\nAnswer: {self.answers[i]}\n"
+
+        messagebox.showinfo("Additional Information", message)
+        self.destroy()
+        self.back_callback()
